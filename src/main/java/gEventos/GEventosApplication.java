bdb5d9bacf3bc599b26772f2d33cf6a587024d09
Gabriel Xavier <gabriel202510464@grad.unifafire.edu.br>
2026-09-01 12:32:25 -0300

    Initial commit

---
+++ src/main/java/gEventos/GEventosApplication.java
@@ -0,0 +1,13 @@
+package gEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GEventosApplication.class, args);
+	}
+
+}
