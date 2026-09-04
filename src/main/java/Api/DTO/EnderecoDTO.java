275e4661112dc400c024d021933078ee3f68b9a9
Gabriel Xavier <gabriel202510464@grad.unifafire.edu.br>
2026-09-04 19:41:56 -0300

    feat(location-dto): create enderecoDto for location data

---
+++ src/main/java/Api/DTO/EnderecoDTO.java
@@ -0,0 +1,56 @@
+package Api.DTO;
+
+public class EnderecoDTO {
+    private String nomeEndereco;
+    private int numeroEndereco;
+    private String cidadeEndereco;
+    private String estado;
+    private String pais;
+
+    public EnderecoDTO(String nomeEndereco, int numeroEndereco, String cidadeEndereco, String estado, String pais){
+        setNomeEndereco(nomeEndereco);
+        setNumeroEndereco(numeroEndereco);
+        setCidadeEndereco(cidadeEndereco);
+        setEstado(estado);
+        setPais(pais);
+    }
+    public String getCidadeEndereco() {
+        return cidadeEndereco;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public int getNumeroEndereco() {
+        return numeroEndereco;
+    }
+
+    public String getNomeEndereco() {
+        return nomeEndereco;
+    }
+
+    private void setCidadeEndereco(String cidadeEndereco) {
+        this.cidadeEndereco = cidadeEndereco;
+    }
+
+    private void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    private void setNumeroEndereco(int numeroEndereco) {
+        this.numeroEndereco = numeroEndereco;
+    }
+
+    private void setNomeEndereco(String nomeEndereco){
+        this.nomeEndereco = nomeEndereco;
+    }
+
+    private void setPais(String pais) {
+        this.pais = pais;
+    }
+}
