555b98a5000bf3f27cfc594b699e7fd73ba1e5ec
Gabriel El  Xavier <gabriel202510464@grad.unifafire.edu.br>
2026-09-14 12:30:34 -0300

    feat: create client service

--- src/main/java/Api/Service/ClienteService.java
+++ src/main/java/Api/Service/ClienteService.java
@@ -2,28 +2,36 @@ package Api.Service;
 
 import Api.Model.ClienteModel;
 import Api.Model.EnderecoModel;
+import Api.Service.Exceptions.ContaNaoEncontrada;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@RequestMapping("/c")
-public class ClientService {
+public class ClienteService {
 
-    private final EnderecoModel enderecoModel = new EnderecoModel("Rua da Facada", 12, "Recife", "Pernambuco", "Brasil");
-    private final List<ClienteModel> listaClientes = List.of(
-            new ClienteModel("Garbagoob", 16, enderecoModel),
-            new ClienteModel("AutroHungaro", 90, enderecoModel)
-    );
+//    private final EnderecoModel enderecoModel = new EnderecoModel("Rua da Facada", 12, "Recife", "Pernambuco", "Brasil");
+//    private final List<ClienteModel> listaClientes = List.of(
+//            new ClienteModel("Garbagoob", 16L),
+//            new ClienteModel("AutroHungaro", 90L)
+//    );
+//
+    private List<ClienteModel> listaDeClientes = new ArrayList<>();
 
-    @GetMapping("/{idade}")
-    public ClienteModel buscaPorIdade(@PathVariable String idade){
-        return listaClientes.stream()
-                .filter(clienteNaLista  -> clienteNaLista.getIdade() == Integer.parseInt(idade))
-                .findFirst()
-                .orElseThrow(() -> new ArithmeticException(idade));
+    public List<ClienteModel> getAll(){
+        return listaDeClientes;
     }
+
+    public ClienteModel buscaPorIdade(String nome){
+        return listaDeClientes.stream()
+                .filter(c  -> c.getNome().equals(nome))
+                .findFirst()
+                .orElseThrow(() -> new ContaNaoEncontrada("A conta não está aqui irmão"));
+    }
+
+    public void adicionarCliente(String nome, int idade){
+        listaDeClientes.add(new ClienteModel(nome, idade));
+    }
+
 }
---
+++ src/main/java/Api/Service/Exceptions/ContaNaoEncontrada.java
@@ -0,0 +1,7 @@
+package Api.Service.Exceptions;
+
+public class ContaNaoEncontrada extends RuntimeException {
+    public ContaNaoEncontrada(String message) {
+        super(message);
+    }
+}
